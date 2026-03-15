4de27a7e80512a9e14fdad9e8290869942547929
aliabbas-rashid <aliabbasrashid@gmail.com>
2026-03-15 23:59:38 +0000

    Update run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /Users/aliabbasrashid/PycharmProjects/poke-tracker/backend
+path="ENTER_PATH_TO_YOUR_PROJECT_FOLDER_HERE"
+path="$path/poke-tracker/backend"
+cd $path || { echo "Path Failure"; exit 1; }
 source .venv/bin/activate
 python -c "import sys; print('Python:', sys.version); print('Executable:', sys.executable)"
 python -c "from app.main import app; print('App loaded:', app)"
