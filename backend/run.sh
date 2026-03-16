14e0553e9cee771365982b8a8ef37794b2058640
aliabbas-rashid <aliabbasrashid@gmail.com>
2026-03-16 00:01:05 +0000

    Update run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-path="ENTER_PATH_TO_YOUR_PROJECT_FOLDER_HERE"
+
+# Path example for MacOS, adjust as needed for other operating systems: /home/username/projects
 path="$path/poke-tracker/backend"
+
 cd $path || { echo "Path Failure"; exit 1; }
 source .venv/bin/activate
 python -c "import sys; print('Python:', sys.version); print('Executable:', sys.executable)"
 python -c "from app.main import app; print('App loaded:', app)"
 uvicorn app.main:app --reload --port 8000
-
